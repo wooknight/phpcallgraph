66c73243976ad83b1aa92045206ae9c30b02a177
fakko <fakko@7200729f-c539-0410-9c6d-c969e6bca738>
2007-10-11 07:13:02 +0000

    autoload files for eZ Components and InstantSVC directly included   (improves Windows compatibility)
    git-svn-id: svn+ssh://svn.code.sf.net/p/phpcallgraph/code/trunk@9 7200729f-c539-0410-9c6d-c969e6bca738

---
+++ lib/ezcomponents-instantsvc/components/autoload/code_analyzer_autoload.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Autoloader definition for the File component.
+ *
+ * @copyright Copyright (C) 2005, 2006 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package CodeAnalyzer
+ */
+
+return array(
+    'iscCodeAnalyzer' => 'CodeAnalyzer/code_analyzer.php',
+    'iscCodeAnalyzerClassLoader' => 'CodeAnalyzer/class_loader.php',
+    'iscCodeAnalyzerFileDetails' => 'CodeAnalyzer/file_details.php'
+);
+
+?>
