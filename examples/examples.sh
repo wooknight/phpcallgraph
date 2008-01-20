627b4cd7163e15207483c0d479fd58799c721233
fakko <fakko@7200729f-c539-0410-9c6d-c969e6bca738>
2008-01-20 23:00:45 +0000

    - Example shell script changed
    git-svn-id: svn+ssh://svn.code.sf.net/p/phpcallgraph/code/trunk@45 7200729f-c539-0410-9c6d-c969e6bca738

--- examples/examples.sh
+++ examples/examples.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ../bin/phpcallgraph -n -f png -o PHPCallGraph.png ../src/PHPCallGraph.php
-../bin/phpcallgraph -f png -o phpcallgraph-library.png ../src/drivers/ ../src/PHPCallGraph.php
+../bin/phpcallgraph -f png -o phpcallgraph-library.png ../src/PHPCallGraph.php ../src/drivers/CallgraphDriver.php ../src/drivers/TextDriver.php ../src/drivers/GraphVizDriver.php ../lib/pear/Image/GraphViz.php
 ../bin/phpcallgraph -r -f png -o phpcallgraph-src.png ../src/
 ../bin/phpcallgraph -r -f png -o phpcallgraph.png ../src/ ../lib/
 ../bin/phpcallgraph -p -f png -o testfiles.png ../test/testfiles
