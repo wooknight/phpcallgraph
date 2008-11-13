f57e35946eabf07623563749042747e2e70cd56f
fakko <fakko@7200729f-c539-0410-9c6d-c969e6bca738>
2008-11-13 15:42:09 +0000

    - Added code to automatically find an SVN, Release or PEAR version of the ezcBase class - Added an __autoload function using ezcBase to load eZ Components classes
    git-svn-id: svn+ssh://svn.code.sf.net/p/phpcallgraph/code/trunk@59 7200729f-c539-0410-9c6d-c969e6bca738

---
+++ src/autoload-ezcomponents.php
@@ -0,0 +1,18 @@
+<?php
+// try to find an SVN, Release or PEAR version of base.php
+foreach (array('Base/src/base.php', 'Base/base.php', 'ezc/Base/base.php') as $ezcBaseFileToInclude) {
+    if (!in_array('ezcBase', get_declared_classes())) {
+        @include_once $ezcBaseFileToInclude;
+    } else {
+        break;
+    }
+}
+// remove the global variable used in the foreach loop
+unset($ezcBaseFileToInclude);
+
+// define an __autoload function which is automatically called in case a class
+// is used which hasn't been declared
+function __autoload( $className ) {
+    ezcBase::autoload( $className );
+}
+?>
