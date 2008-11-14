6ad32e2ddc59bf04a710dd23d4cfafa4727219c3
fakko <fakko@7200729f-c539-0410-9c6d-c969e6bca738>
2008-11-14 16:17:24 +0000

    - Added the autoloader to the eZ Components example
    git-svn-id: svn+ssh://svn.code.sf.net/p/phpcallgraph/code/trunk@65 7200729f-c539-0410-9c6d-c969e6bca738

--- examples/ezcomponents.php
+++ examples/ezcomponents.php
@@ -1,19 +1,21 @@
 <?php
+// change this path to your eZ Components SVN working copy
+// also change it in ezcomponents-autoload.php
+$ezcDir = '../../../../instantsvc/ezc-svn/trunk';
+
 // set up include path
 set_include_path(
     realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR
     . realpath(dirname(__FILE__) . '/../lib/ezcomponents-instantsvc/components') . PATH_SEPARATOR
-    . ini_get( "include_path" ) . PATH_SEPARATOR
+    . get_include_path() . PATH_SEPARATOR
     . realpath(dirname(__FILE__) . '/../lib/pear')
 );
 
 // configure eZ Components autoloader
-// required because of PHPCallGraphCli using eczConsoleTools
-// and PHPCallGraph leveraging InstantSVC CodeAnalyzer and Extended Reflection API
-require_once 'Base/src/base.php';
-function __autoload( $className ) { ezcBase::autoload( $className ); }
+// required because of phpCallGraph leveraging InstantSVC CodeAnalyzer and Extended Reflection API
+require_once 'autoload-ezcomponents.php';
 
-// include PHPCallGraph library
+// include phpCallGraph library
 require_once 'PHPCallGraph.php';
 
 // configure output driver
@@ -22,9 +24,11 @@ $driver = new GraphVizDriver('png');
 
 // configure generator
 $phpcg = new PHPCallGraph($driver);
+$phpcg->setDebug(true);
 
-// change this path to your eZ Components SVN working copy
-$ezcDir = '../../../../instantsvc/ezc-svn/trunk';
+// set a PHP file conaining an __autoload function which will be included
+// in the sandbox of the InstantSVC CodeAnalyzer
+$phpcg->setAutoloadFile('ezcomponents-autoload.php'); // will be found since the current include path is propagated into the sandbox
 
 // destination directory for the output files
 $targetDir = '../../website/htdocs/examples/ezcomponents';
@@ -44,6 +48,10 @@ foreach ($componentDirs as $componentDir) {
 
         // output result
         $phpcg->save("$targetDir/$componentName.png");
+
+        //TODO: output SVGg and reder PNG with Inkscape
+        //inkscape --without-gui --export-background=#ffffff --export-area-drawing --export-dpi=72 --export-png=a.png phpdoc-full.dot.svg
+        // for thumbnails: --export-width=980
 
         // initiallize a new graph
         $driver->reset();
