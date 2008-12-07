e6f98792e50668c058da6089d732156321ce5b46
fakko <fakko@7200729f-c539-0410-9c6d-c969e6bca738>
2008-12-07 13:27:03 +0000

    - Added a shell script to set up autoloading for the external libraries
    git-svn-id: svn+ssh://svn.code.sf.net/p/phpcallgraph/code/trunk@86 7200729f-c539-0410-9c6d-c969e6bca738

---
+++ setup-env.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp lib/ezcomponents/Base/src/base_autoload.php lib/ezcomponents/autoload/
+cp lib/ezcomponents/ConsoleTools/src/console_autoload.php lib/ezcomponents/autoload/
+cp lib/ezcomponents/Reflection/src/reflection_autoload.php lib/ezcomponents/autoload/
+cp lib/instantsvc/components/CodeAnalyzer/src/code_analyzer_autoload.php lib/instantsvc/components/autoload/
