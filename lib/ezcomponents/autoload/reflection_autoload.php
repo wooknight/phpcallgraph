21c61b2865cb086dc4f11d4070fb6e2bd1ea8672
fakko <fakko@7200729f-c539-0410-9c6d-c969e6bca738>
2008-12-07 14:04:01 +0000

    - Autoloading definitions for the current eZ Components
    git-svn-id: svn+ssh://svn.code.sf.net/p/phpcallgraph/code/trunk@89 7200729f-c539-0410-9c6d-c969e6bca738

---
+++ lib/ezcomponents/autoload/reflection_autoload.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Autoloader definition for the Reflection component.
+ *
+ * @copyright Copyright (C) 2005-2007 eZ systems as. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ * @version //autogentag//
+ * @filesource
+ * @package Reflection
+ */
+
+return array(
+    'ezcReflectionType'            => 'Reflection/interfaces/type.php',
+    'ezcReflectionAbstractType'    => 'Reflection/types/abstract_type.php',
+    'ezcReflectionClass'           => 'Reflection/class.php',
+    'ezcReflectionDocParser'       => 'Reflection/interfaces/parser.php',
+    'ezcReflectionDocTag'          => 'Reflection/phpdoc/tag.php',
+    'ezcReflectionTypeFactory'     => 'Reflection/interfaces/type_factory.php',
+    'ezcReflectionApi'             => 'Reflection/reflection.php',
+    'ezcReflectionArrayType'       => 'Reflection/types/array_type.php',
+    'ezcReflectionClassType'       => 'Reflection/types/class_type.php',
+    'ezcReflectionDocTagFactory'   => 'Reflection/phpdoc/tag_factory.php',
+    'ezcReflectionDocTagParam'     => 'Reflection/tags/param.php',
+    'ezcReflectionDocTagReturn'    => 'Reflection/tags/return.php',
+    'ezcReflectionDocTagVar'       => 'Reflection/tags/var.php',
+    'ezcReflectionDocTagparam'     => 'Reflection/tags/param.php',
+    'ezcReflectionDocTagreturn'    => 'Reflection/tags/return.php',
+    'ezcReflectionDocTagvar'       => 'Reflection/tags/var.php',
+    'ezcReflectionExtension'       => 'Reflection/extension.php',
+    'ezcReflectionFunction'        => 'Reflection/function.php',
+    'ezcReflectionMethod'          => 'Reflection/method.php',
+    'ezcReflectionParameter'       => 'Reflection/parameter.php',
+    'ezcReflectionPhpDocParser'    => 'Reflection/phpdoc/parser.php',
+    'ezcReflectionPrimitiveType'   => 'Reflection/types/primitive_type.php',
+    'ezcReflectionProperty'        => 'Reflection/property.php',
+    'ezcReflectionTypeFactoryImpl' => 'Reflection/type_factory.php',
+    'ezcReflectionTypeMapper'      => 'Reflection/type_mapper.php',
+);
+?>
