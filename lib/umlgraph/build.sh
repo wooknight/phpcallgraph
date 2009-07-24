38f0fc99ff1d03fd37f3abe2ad2635b0e0d6fe2e
mrjc <mrjc@7200729f-c539-0410-9c6d-c969e6bca738>
2009-07-24 11:36:23 +0000

    Added UMLGraph libraries
    git-svn-id: svn+ssh://svn.code.sf.net/p/phpcallgraph/code/trunk@158 7200729f-c539-0410-9c6d-c969e6bca738

---
+++ lib/umlgraph/build.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+echo "READ this file $0 and execute the steps contained in it by hand"
+
+#cd lib/umlgraph
+
+#cp /usr/local/src/UMLGraph-5.2/UMLGraph-5.2.tar.gz .
+#cp /usr/local/src/UMLGraph-5.2/lib/sequence.pic .
+
+#cp /usr/local/src/UMLGraph-5.2/LICENSE .
+#cp /usr/local/src/UMLGraph-5.2/README.txt .
+
+#mv README.txt ORIGINAL-README.txt
+
+#echo "UMLGraph can be found at http://www.umlgraph.org. PHPCallGraph needs only sequence.pic" > README.TXT
